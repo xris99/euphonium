d3f61380f99b4a8c64697d8eaaf64e9320a0bf28
xris99 <79798089+xris99@users.noreply.github.com>
2023-03-15 23:27:14 +0100

    Update Core.cpp

--- euphonium/src/Core.cpp
+++ euphonium/src/Core.cpp
@@ -25,7 +25,7 @@ Core::Core() : bell::Task("Core", 4 * 1024, 2, 0) {
 
     // Add preincluded audio processors
     audioProcessor->addProcessor(std::make_unique<UserDSPProcessor>());
-    //audioProcessor->addProcessor(std::make_unique<SoftwareVolumeProcessor>());
+    audioProcessor->addProcessor(std::make_unique<SoftwareVolumeProcessor>());
     audioProcessor->addProcessor(std::make_unique<EqualizerProcessor>());
 
     // include berry
